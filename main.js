c61e203d08eaccea2821ed9bcfabf8aaef81466a
Shio <shio7113@gmail.com>
2021-01-19 14:40:31 +0900

    heron added

--- main.js
+++ main.js
@@ -30,7 +30,19 @@ exports.Dbtp = (x1,y1,x2,y2) => {
 }
 
 // Area things
+
+// Triangle
 exports.Aotw3p = (x1,y1,x2,y2,x3,y3) => {
     let value = 0.5*Math.abs(x1*y2 + x2*y3 + x3*y1 - x2*y1 - x3*y2 - x1*y3)
     return value;
+}
+
+exports.Aotw3ls = (l1, l2, l3) => {
+    let s = 0.5*(l1+l2+l3)
+    let value = Math.sqrt(s*((s-l1)+(s-l2)+(s-l3)))
+    return value;
+}
+// Circle
+exports.AoC = (r) => {
+    return Math.pow(r,2)*Math.PI
 }
