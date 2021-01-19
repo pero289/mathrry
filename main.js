13947af06a6479b99e87c4d9d0bd57bc399b62dc
Shio <shio7113@gmail.com>
2021-01-19 13:52:31 +0900

    a

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+exports.add = (a,b) => {
+    return a+b;
+}
+
+exports.sub = (a,b) => {
+    return a-b;
+}
+
+exports.divi = (a,b) => {
+    return a/b;
+}
+
+exports.multi = (a,b) => {
+    return a*b;
+}
+
+exports.revdivi = (a,b) => {
+    return b/a;
+}
+
+exports.revmulti = (a,b) => {
+    return b*a;
+}
