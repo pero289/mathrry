ed5f9c4ff713b2996ce915f8052888021ed30470
Shio <shio7113@gmail.com>
2021-01-19 14:19:47 +0900

    0.0.5

--- main.js
+++ main.js
@@ -23,8 +23,14 @@ exports.revmulti = (a,b) => {
     return b*a;
 }
 
-//
-exports.dbp = (x1,y1,x2,y2) => {
+// Coordinate things
+exports.Dbtp = (x1,y1,x2,y2) => {
     let value = Math.sqrt(Math.pow(x1-x2,2) + Math.pow(y1-y2,2))
+    return value;
+}
+
+// Area things
+exports.Aotw3p = (x1,y1,x2,y2,x3,y3) => {
+    let value = 0.5*Math.abs(x1*y2 + x2*y3 + x3*y1 - x2*y1 - x3*y2 - x1*y3)
     return value;
 }
